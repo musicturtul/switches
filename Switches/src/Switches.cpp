7c51df02ab8dd6aa4742ce61af770e9b64a0e76f
Kevin Hallman <musicturtul@192.168.1.103>
2018-06-04 21:17:34 -0400

    Added a fruit choice switch, where you choose a fruit and it displays

--- Switches/src/Switches.cpp
+++ Switches/src/Switches.cpp
@@ -36,5 +36,27 @@ int main() {
 	}
 
 	//switches cannot use string values
+	int fruitChoice;
+	do {
+		cout << "Please choose a fruit" << endl <<"1. Banana" << endl << "2. Cantaloupe" << endl << "3. Watermelon" << endl;
+		cin >> fruitChoice;
+			if (fruitChoice > 3 || fruitChoice < 1 ){
+				cout << "Please enter a valid number" << endl;
+			}
+	} while (fruitChoice > 3 || fruitChoice < 1);
+
+	switch (fruitChoice) {
+	case 1:
+		cout << "You have chosen bananas" << endl;
+		break;
+	case 2:
+		cout << "You have chosen cantaloupe" << endl;
+		break;
+	case 3:
+		cout << "You have chosen watermelon" << endl;
+		break;
+	default:
+		cout << "Please enter a valid fruit choice" << endl;
+	}
 	return 0;
 }

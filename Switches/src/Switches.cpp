d2338e3b7a9157fad22b5e81dd3eebe76b525e4f
Kevin Hallman <musicturtul@192.168.1.103>
2018-06-04 20:36:22 -0400

    Creating the main project

---
+++ Switches/src/Switches.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Switches.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int value = 4;
+
+	switch(value) {
+	//cases can use constants or literal values, but not variables
+	case 4:
+		cout << "Value is 4" << endl;
+		//breaks are used to stop the code from running into the next case
+		break;
+	case 5:
+		cout << "Value is 5" << endl;
+		break;
+	case 6:
+		cout << "Value is 6" << endl;
+		break;
+	//use a default case so that something happens even if no cases match
+	default:
+		cout << "Unrecognized value" << endl;
+	}
+	return 0;
+}
